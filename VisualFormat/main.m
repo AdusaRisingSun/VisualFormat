c3bda3f3ae74240088db171e068f4916a8962355
AdusaRisingSun <Adusa@AdusadeMac-mini.local>
2015-09-08 14:23:01 +0800

    Initial Commit

---
+++ VisualFormat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VisualFormat
+//
+//  Created by Adusa on 15/9/8.
+//  Copyright (c) 2015年 Adusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
